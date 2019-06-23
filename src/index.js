053cd2c1e0223bf28787339e915b3d53115a910b
Chris Shaffer <chris@methodbox.io>
2019-06-23 12:11:27 -0500

    adding styling

--- src/index.js
+++ src/index.js
@@ -1,11 +1,33 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import "../node_modules/materialize-css/dist/css/materialize.min.css";
 
 class App extends React.Component {
   render() {
     return (
-      <div>
-        <h1>Hello, World.</h1>
+      <div className="container">
+        <h1 className="center">Hello, World.</h1>
+        <div className="row">
+          <div className="col s6 offset-s3">
+            <div className="card">
+              <div className="card-content">
+                <span className="card-title">Parcel Example</span>
+                <p>
+                  I built this Parcel example app in about 5 minutes. Click the
+                  link below to learn more.
+                </p>
+              </div>
+              <div className="card-action grey lighten-4">
+                <a
+                  className="blue-text .text-accent-3"
+                  href="https://github.com/methodbox/parcel-example"
+                >
+                  Learn More
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
